16f4030ac7b4ee08bd34094adb140617bcf6f161
Tyler <tuh26432@temple.edu>
2020-05-13 23:04:28 -0400

    login update

--- login.js
+++ login.js
@@ -6,7 +6,7 @@ function onSignIn(googleUser) {
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     console.log("version(1.0.2)");
-    window.location.href =  './index.html'
+    // window.location.href =  './index.html'
   }
 
 function signOut() {
